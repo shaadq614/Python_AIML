e791ced912c877673d4abdb1551d067d80d441a8
shaadq614 <mohdshaadqureshi134@gmail.com>
2026-07-18 19:02:36 +0530

    Main

---
+++ datatype.py
@@ -0,0 +1,34 @@
+# # #14/07/26 - FullStack Guru
+
+# # #Day -1 :
+age = 73
+height = 5.8
+print(age)
+print(height)
+
+name = "Shaad Qureshi"
+print(name)
+
+isokay=True
+print(isokay)
+
+result = None
+print(result)
+
+a = input("Your name:")
+
+x = input("Enter first number:")
+y = input("Enter second number:")
+x1 = float(x)
+y1 = float(y)
+z= x1+y1
+print(z)
+
+marks = float(input("Enter your marks:"))
+if marks >= 33 :
+    print("You have passed")
+else : 
+    print("You have failed")
+
+x= 50
+print(type(x))
