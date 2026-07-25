2c138a2c1646569f0bd287697ff0235023e76995
shaadq614 <mohdshaadqureshi134@gmail.com>
2026-07-26 01:55:52 +0530

    String

---
+++ string.py
@@ -0,0 +1,28 @@
+# var = input("Enter you name : ")
+# print(f"Hello {var}") #format string
+
+a = "Shaad"
+print(a.isdigit())
+
+b = "7868"
+print(b.isalpha())
+
+c = "shaad614"
+print(c.isalnum())
+
+d = " "
+print(d.isspace())
+
+car = input("Enter something : ")
+rat = car.isspace()
+print(rat)
+
+tab = "Scott"
+print(tab.lower())
+
+bat = "BATMAN"
+print(bat.upper())
+
+cat = "CAT WOMAN"
+key = cat.split()
+print(key)
